75d385519500e5d9c7162305c699dd7f0efc15d9
Sebastian Twarog <sebastian.twarog1989@gmail.com>
2023-09-04 15:24:07 +0200

    Create product api test

--- tests/Application/ApplicationTestCase.php
+++ tests/Application/ApplicationTestCase.php
@@ -9,20 +9,36 @@ namespace App\Tests\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 abstract class ApplicationTestCase extends KernelTestCase
 {
     protected HttpClientInterface $client;
+
+    protected array $headers = [
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+    ];
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->client = HttpClient::createForBaseUri(baseUri: 'http://web', defaultOptions: [
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ]
+            'headers' => $this->headers,
+        ]);
+    }
+
+    protected function iAmAuthenticated(): self
+    {
+        $this->headers['authentication'] = '';
+        return $this;
+    }
+
+    protected function iCallPostApi(string $endpoint, array $payload): ResponseInterface
+    {
+        return $this->client->request('POST', '/api/products', [
+            'json' => $payload,
         ]);
     }
 }
--- tests/Application/Catalog/CreateProductControllerTest.php
+++ tests/Application/Catalog/CreateProductControllerTest.php
@@ -14,37 +14,43 @@ final class CreateProductControllerTest extends ApplicationTestCase
 {
     public function testCreateSuccessfully(): void
     {
-        // When I try to create a new product that does not exist yet
-        $response = $this->client->request('POST', '/api/products', [
-            'json' => [
-                'name' => 'fake product',
-                'proteins' => 12.0,
-                'fats' => 7.2,
-                'carbs' => 62.0,
-                'kcal' => 375,
-                'producerName' => 'producer',
-            ],
+        /*
+         * I am authenticated
+         * When I try to create a new product that does not exist yet
+         * Then I should see created status code
+         */
+        $this->iAmAuthenticated();
+
+        $response = $this->iCallPostApi('/api/products', [
+            'name' => 'fake product',
+            'proteins' => 12.0,
+            'fats' => 7.2,
+            'carbs' => 62.0,
+            'kcal' => 375,
+            'producerName' => 'producer',
         ]);
 
-        // Then I should see created status code
         $this->assertEquals(Response::HTTP_CREATED, $response->getStatusCode());
     }
 
     public function testFailsOnDuplication(): void
     {
-        // When I try to create a new product that does not exist yet
-        $response = $this->client->request('POST', '/api/products', [
-            'json' => [
-                'name' => 'fake product',
-                'proteins' => 12.0,
-                'fats' => 7.2,
-                'carbs' => 62.0,
-                'kcal' => 375,
-                'producerName' => 'producer',
-            ],
+        /*
+         * I am authenticated
+         * When I try to create a new product that does exist already with the same name
+         * Then I should see conflict status code
+         */
+        $this->iAmAuthenticated();
+
+        $response = $this->iCallPostApi('/api/products', [
+            'name' => 'fake product',
+            'proteins' => 12.0,
+            'fats' => 7.2,
+            'carbs' => 62.0,
+            'kcal' => 375,
+            'producerName' => 'producer',
         ]);
 
-        // Then I should see conflict status code
         $this->assertEquals(Response::HTTP_CONFLICT, $response->getStatusCode());
     }
 }
