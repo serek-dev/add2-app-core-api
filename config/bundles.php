50b63a7db7680fdfec59da0594d8ecae6751af08
Sebastian Twaróg <sebastian.twarog1989@gmail.com>
2023-10-29 13:31:59 +0100

    Installed monolog & logger

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
