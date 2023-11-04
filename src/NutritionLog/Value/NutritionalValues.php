6cb4ee647a906326bb8660d1b8dded848edfadc7
Sebastian Twaróg <sebastian.twarog1989@gmail.com>
2023-11-04 23:54:20 +0100

    NutritionalValues.php should be defined with default as it's emedable

--- src/NutritionLog/Value/NutritionalValues.php
+++ src/NutritionLog/Value/NutritionalValues.php
@@ -12,16 +12,16 @@ use Doctrine\ORM\Mapping\Embeddable;
 #[Embeddable]
 final class NutritionalValues
 {
-    #[Column(nullable: true)] private readonly float $proteins;
-    #[Column(nullable: true)] private readonly float $fats;
-    #[Column(nullable: true)] private readonly float $carbs;
+    #[Column(nullable: true)] private float $proteins = 0.0;
+    #[Column(nullable: true)] private float $fats = 0.0;
+    #[Column(nullable: true)] private float $carbs = 0.0;
 
     public function __construct(
         Weight                 $proteins,
         Weight                 $fats,
         Weight                 $carbs,
         #[Column(nullable: true)]
-        private readonly float $kcal,
+        private readonly float $kcal = 0.0,
     )
     {
         $this->carbs = $carbs->getRaw();
